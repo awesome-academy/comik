c34cfcac1bc4ad6de29a12713de2b6be088bee29
QuynhTrang2508 <uetquynhtrang2508@gmail.com>
2021-04-15 13:16:14 +0700

    task#35289

--- app/src/main/java/com/example/comik/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/comik/ui/setting/SettingFragment.kt
@@ -3,4 +3,5 @@ package com.example.comik.ui.setting
 import androidx.fragment.app.Fragment
 import com.example.comik.R
 
-class SettingFragment : Fragment(R.layout.fragment_home)
+class SettingFragment : Fragment(R.layout.fragment_setting)
+
