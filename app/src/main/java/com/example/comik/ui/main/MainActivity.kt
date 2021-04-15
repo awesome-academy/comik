ab333b678ae0418380e2b703201028573192310b
QuynhTrang2508 <uetquynhtrang2508@gmail.com>
2021-04-15 13:53:50 +0700

    task35289

--- app/src/main/java/com/example/comik/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/comik/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.comik.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.comik.R
@@ -10,7 +11,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Theme_Comik)
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme)
+        } else {
+            setTheme(R.style.Theme_Comik)
+        }
         setContentView(R.layout.activity_main)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentNavHost) as NavHostFragment
--- app/src/main/java/com/example/comik/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/comik/ui/setting/SettingFragment.kt
@@ -1,7 +1,55 @@
 package com.example.comik.ui.setting
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.example.comik.R
+import com.example.comik.utils.loadLocalImageCircle
+import kotlinx.android.synthetic.main.fragment_setting.*
 
-class SettingFragment : Fragment(R.layout.fragment_setting)
+class SettingFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_setting, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        imageLogo.loadLocalImageCircle(R.drawable.splash)
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            switchTheme.isChecked = true
+        }
+        changeMode()
+        viewInfo.setOnClickListener {
+            showDialogInfo()
+        }
+    }
+
+    private fun changeMode() {
+        switchTheme.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+
+    private fun showDialogInfo() {
+        context?.let {
+            AlertDialog.Builder(it)
+                .setTitle(resources.getString(R.string.text_about_app))
+                .setMessage(resources.getString(R.string.text_message_info))
+                .setNegativeButton(resources.getString(R.string.action_ok)) { dialog, _ ->
+                    dialog.cancel()
+                }
+                .create()
+                .show()
+        }
+    }
+}
